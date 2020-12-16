9236b3d8372dcf23d87129e68018a06577bff5cb
Mason Embry <mason@embrycode.com>
2020-12-16 10:25:21 -0600

    Add health check

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const apiStatusEl = document.getElementById("api-status");
+
+const isProd = false;
+const API_URL = isProd
+  ? "http://dev.embrycode.com/api"
+  : "http://localhost:3000";
+
+fetch(`${API_URL}/health-check`)
+  .then((res) => {
+    const isHealthy = res.status === 200;
+
+    setApiStatus(isHealthy);
+  })
+  .catch((err) => {
+    console.error(err);
+
+    setApiStatus(false);
+  });
+
+function setApiStatus(isHealthy) {
+  apiStatusEl.innerHTML = isHealthy
+    ? '<span class="api-status up">Up</span>'
+    : '<span class="api-status down">Down</span>';
+}
